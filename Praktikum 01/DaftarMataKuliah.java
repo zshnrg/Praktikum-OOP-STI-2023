3c442d5a68a8df86360ffb02bcffd95f74b2c3c6
zshnrg <16521411@mahasiswa.itb.ac.id>
2023-01-31 12:54:06 +0700

    Add praktikum 01

---
+++ Praktikum 01/DaftarMataKuliah.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class DaftarMataKuliah {
+	Scanner scan = new Scanner(System.in);
+	
+	public static void main (String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		String kode = scan.nextLine();
+		String namaMk = scan.nextLine();
+		int sks = scan.nextInt();
+		
+		scan.nextLine();
+		String namaDsn = scan.nextLine();
+		
+		int cap = scan.nextInt();
+		int rev = scan.nextInt();
+		
+		MataKuliah mk = new MataKuliah(kode, namaMk, sks, namaDsn, cap);
+		
+		
+		System.out.println("Kode Mata Kuliah: " + mk.getKode());
+		System.out.println("Nama Mata Kuliah: " + mk.getNama());
+		System.out.println("SKS Mata Kuliah: " + mk.getSks());
+		System.out.println("Nama Dosen: " + mk.getDosen());
+		System.out.println("Kapasitas Kuliah: " + mk.getKapasitas());
+		mk.tambahKapasitas(rev);
+		System.out.println("Total Revisi Kapasitas Kuliah: " + mk.getKapasitas());
+		scan.close(); 
+	}
+}
