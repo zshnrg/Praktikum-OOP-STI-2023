b853f31d3e291432379cff0dc233bace4cffbca8
zshnrg <16521411@mahasiswa.itb.ac.id>
2023-04-11 10:17:47 +0700

    Update solusi lebih efisien

--- Praktikum 09/String/Main.java
+++ Praktikum 09/String/Main.java
@@ -2,18 +2,10 @@ import java.util.*;
 
 public class Main {
     public static List<String> findCommonElements(List<String> list1, List<String> list2) {
-        TreeSet<String> word2 = new TreeSet<String>();
-
-        for (String l2 : list2) {
-            word2.add(l2);
-        }
-
-        TreeSet<String> result = new TreeSet<String>();
         List<String> res = new ArrayList<String>();
 
         for (String l1 : list1) {
-            if (word2.contains(l1) && !result.contains(l1)) {
-                result.add(l1);
+            if (list2.contains(l1) && !res.contains(l1)) {
                 res.add(l1);
             }
         }
