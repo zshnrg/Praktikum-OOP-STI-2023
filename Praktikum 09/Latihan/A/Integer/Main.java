25303051e1375656f40e54b05d79040c31b15cc2
ojan <16521411@mahasiswa.itb.ac.id>
2023-05-18 18:29:26 +0700

    Fixed WA, 100% score

--- Praktikum 09/Latihan/A/Integer/Main.java
+++ Praktikum 09/Latihan/A/Integer/Main.java
@@ -3,7 +3,14 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] input = sc.nextLine().split("\\s+");
+        String scan = sc.nextLine();
+
+        if (scan.isEmpty()) {
+            System.out.println("[]");
+            return;
+        }
+
+        String[] input = scan.split("\\s+");
         
         Set<Integer> set = new HashSet<Integer>();
         for (String in : input) {
